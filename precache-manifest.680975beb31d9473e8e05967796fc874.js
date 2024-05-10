02da66a0ee7affcb2ad1c559acae021ef9e6bcc3
Camilo <juaancamilo82@outlook.com>
2024-05-10 13:48:40 -0500

    Updates

---
+++ precache-manifest.680975beb31d9473e8e05967796fc874.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740c7ce46b390907ecc02de0b0022f96",
+    "url": "/portafolio.github.io/index.html"
+  },
+  {
+    "revision": "3c5e7659e1de8b8444fb",
+    "url": "/portafolio.github.io/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "620fcfae7041bc27fed0",
+    "url": "/portafolio.github.io/static/css/main.9893f67a.chunk.css"
+  },
+  {
+    "revision": "3c5e7659e1de8b8444fb",
+    "url": "/portafolio.github.io/static/js/2.162d1574.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/portafolio.github.io/static/js/2.162d1574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620fcfae7041bc27fed0",
+    "url": "/portafolio.github.io/static/js/main.8c992563.chunk.js"
+  },
+  {
+    "revision": "e2c056effd60de4c4032",
+    "url": "/portafolio.github.io/static/js/runtime-main.3c5230b3.js"
+  }
+]);
