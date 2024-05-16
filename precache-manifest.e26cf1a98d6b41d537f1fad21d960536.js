b96a1cd41dc429cc4af453b2393fedb17b026543
Camilo <juaancamilo82@outlook.com>
2024-05-16 15:40:14 -0500

    Updates

--- precache-manifest.e26cf1a98d6b41d537f1fad21d960536.js
+++ precache-manifest.e26cf1a98d6b41d537f1fad21d960536.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "229f33fff59a6a1645dab6493ebe465a",
+    "revision": "0a90ed8a9d98a24a0cbdafac2ca48ceb",
     "url": "/portafolio.github.io/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portafolio.github.io/static/css/2.fd277b09.chunk.css"
   },
   {
-    "revision": "a32e45d214369024a66e",
+    "revision": "2faa988b43154ea37ca7",
     "url": "/portafolio.github.io/static/css/main.d6021523.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portafolio.github.io/static/js/2.e9e89220.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a32e45d214369024a66e",
-    "url": "/portafolio.github.io/static/js/main.da74e069.chunk.js"
+    "revision": "2faa988b43154ea37ca7",
+    "url": "/portafolio.github.io/static/js/main.6b07fb92.chunk.js"
   },
   {
     "revision": "e2c056effd60de4c4032",
